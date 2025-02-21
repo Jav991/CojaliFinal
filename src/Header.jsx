651144213b1c4599fcd9df6e5893728cec5877e9
Jav991 <jgomezcomino04@gmail.com>
2025-02-21 11:37:59 +0100

    Hacemos el header del react

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+function Header(){
+    return(
+    <header>
+        <h1>Esto es un header de mi aplicación</h1>
+        <nav>
+        <ul>
+            <li>Inicio</li>
+            <li>Mascotas</li>
+            <li>Tienda</li>
+        </ul>
+        </nav>
+        <div><span>Usuario</span></div>
+    </header>
+    )
+}
+
+export default Header
